9e35669224656ac253cdf69feede8fe6fe20fbce
aersosi <aersosi@avodaq.com>
2022-04-05 00:38:31 +0200

    [improve] - styling

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,7 +5,7 @@ export default function Button({ text, link }) {
     return (
         <button className="flex">
             <Link
-                className="rounded-md text-white bg-avo-pink-pink hover:bg-avo-pink-500 hover:text-white hover:-translate-y-1 hover:scale-105 drop-shadow-lg hover:drop-shadow-xl transition px-8 py-4 font-bold text-xl"
+                className="text-white bg-avo-pink-pink hover:bg-avo-pink-500 hover:text-white hover:-translate-y-1 hover:scale-105 drop-shadow-lg hover:drop-shadow-xl transition px-8 py-4 font-bold text-xl"
                 to={link}>
                 {text}
             </Link>
