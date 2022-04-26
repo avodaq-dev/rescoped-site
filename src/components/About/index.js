8e1f6ae0dc93f386c426857c5170c75041c59e57
aersosi <aersosi@avodaq.com>
2022-04-26 20:05:28 +0200

    [improve] - styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -18,15 +18,13 @@ export default function About() {
         <section className="px-4 sm:px-16 mx-auto py-32 grid gap-32" id="section-about">
             <article className='grid grid-cols-3 gap-16 lg:gap-24 article-grid'>
                 <Reveal keyframes={customKeyframes} {...fadeUpProps} className={`col-span-3 ease-out`}>
-
                     <h2 className="text-2xl font-bold text-avo-pink-pink">
                         About
                     </h2>
                     {FeatureListAbout.map((props, idx) => (
                         <Feature key={idx} {...props} />
                     ))}
-                    <Button text={'Read more'} link={'#fontend-solutions'}/>
-
+                    <Button text={'Read more'} link={'/blog/'}/>
                 </Reveal>
             </article>
         </section>
